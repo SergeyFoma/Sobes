dffc5bef47861cf194dba6489ca231c0efda3574
SergeyFoma <asdfg2105c@gmail.com>
2026-02-04 14:34:03 +0500

    json pikle

--- pikle/pikle_sobes.py
+++ pikle/pikle_sobes.py
@@ -16,3 +16,16 @@ with open('data.pkl', 'rb') as f:
 # loads() -- из байтов
 restored_data = pickle.loads(serialized)
 print(restored_data)
+
+print('-----------------------')
+
+import os
+a=[1,2,3,'ert']
+with open('D:/python3/SOBES/Sobes/pikle/a.pkl', 'wb')as f:
+    pickle.dump(a, f)
+
+print(os.getcwd()) # выведет текущий рабочий каталог
+
+with open('D:/python3/SOBES/Sobes/pikle/a.pkl', 'rb')as f:
+    load_file=pickle.load(f)
+    print(load_file)
