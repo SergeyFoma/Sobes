19b0e7129956c7d99071661e565b4ba6f2528308
SergeyFoma <asdfg2105c@gmail.com>
2026-02-07 07:37:32 +0500

    asd

---
+++ funct/sys_sobes.py
@@ -0,0 +1,39 @@
+import sys
+
+
+#https://tproger.ru/articles/raspredelenie-pamjati-v-python-skolko-i-v-kakih-sluchajah-zanimajut-tipy-dannyh
+
+'''sys.getrefcount — функция в модуле sys Python, которая возвращает количество ссылок на объект. 
+Это значение используется Python для управления памятью: когда счётчик ссылок становится 0, память, 
+занятая объектом, удаляется.'''
+
+# https://habr.com/ru/articles/951742/
+
+a = 1000
+b = a
+print(sys.getrefcount(1000))
+# 5
+del b
+print(sys.getrefcount(a))
+# 4
+
+print(sys.version) #возвращает строку с версией установленного интерпретатора Python
+
+print(sys.platform) # возвращает название платформы, на которой работает интерпретатор (например, win32, linux, darwin).
+print(sys.version_info) #  Возвращает кортеж с компонентами версии (major, minor, micro, releaselevel, serial). Это предпочтительный способ для программных проверок, так как он исключает необходимость парсинга строки.
+# if sys.version_info >= (3, 8):
+#     print("Эта функция требует Python 3.8 или новее.")
+# else:
+#     print("Ваша версия Python устарела.")
+
+print(f"Скрипт запущен интерпретатором: {sys.executable}")
+
+# print(dir(a))
+# print(id(a))
+# print(a.__dir__())
+
+# cv='234'
+# print('DIR CV: ', dir(cv))
+# print('cv.__dir__ = ', cv.__dir__())
+# c=cv.__add__('45')
+# print(c)
--- task/reduce_func.py
+++ task/reduce_func.py
@@ -1,12 +1,35 @@
 # Склейте элементы списка whoami в строку с помощью функции reduce.
+# from functools import reduce
+
+# whoami = ['Я', ' ', 'п', 'р', 'о', 'г', 'р', 'а', 'м', 'м', 'и', 'с', 'т']
+
+# def glue(x):
+#     res=', '.join(x)
+#     return res.replace(', ', '')
+# #glue(whoami)
+
+# whoami = reduce(glue, whoami)
+# print("WHAOMI: ", whoami)
+
+# ---------------------------------------
+
+'''Используйте map и reduce, чтобы посчитать площадь квартиры, состоящей из комнат rooms.'''
+
 from functools import reduce
 
-whoami = ['Я', ' ', 'п', 'р', 'о', 'г', 'р', 'а', 'м', 'м', 'и', 'с', 'т']
+rooms = [
+    {"name": "кухня", "length": 6, "width": 4},
+    {"name": "комната 1", "length": 5.5, "width": 4.5},
+    {"name": "комната 2", "length": 5, "width": 4},
+    {"name": "комната 3", "length": 7, "width": 6.3},
+]
+def scuare_rooms(x):
+    return x['length']*x['width']
+    
+def scuare(x,y):
+    return y+x
+rooms = list(map(scuare_rooms,rooms))
+print('ROOMS: ', rooms)
 
-def glue(x):
-    res=', '.join(x)
-    return res.replace(', ', '')
-#glue(whoami)
-
-whoami = reduce(glue, whoami)
-print("WHAOMI: ", whoami)
+sq = reduce(scuare,rooms)
+print("SQ: ", sq)
