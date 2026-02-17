1a8d787f7a3bdb550eb5c5b9e42bd5f3406317ee
SergeyFoma <asdfg2105c@gmail.com>
2026-02-17 11:51:13 +0500

    sdf

--- PROMTS/python_netologya/telegram_bot.py
+++ PROMTS/python_netologya/telegram_bot.py
@@ -32,6 +32,31 @@ name = 'Сергей'
 token = "8237776832:AAEFMZBwOOrHCJXUKHKV9JBOjDa08oi2STY" # нужен, что бы зарегистрировать бота на серверах telegram
 bot = telebot.TeleBot(token) # переменная содержит все необходимые функции
 
+HELP='''
+/help - напечатать справку по программе,
+/add - добавить задачу в список (название задачи запрашиваем у пользователя),
+/show - напечатать добавленные задачи по дате, all - напечатать словарь всех задач,
+/random - добавляет случайную задачу на дату "Сегодня".
+'''
+
+tasks={}
+
+def add_todo(date, task):
+    # date = input('Введите дату: ')
+    # task = input("Введите задачу: ")
+    if date in tasks:
+        tasks[date].append(task)
+    else:
+        tasks[date]=[]
+        tasks[date].append(task)
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, HELP)
+
+
+
+'''
 @bot.message_handler(content_types=['text']) # регистрируем функцию в качестве обработчика сообщений типа text
 def echo(message): # функция обработчик
     if name in message.text:
@@ -40,5 +65,7 @@ def echo(message): # функция обработчик
     else:
         bot.send_message(message.chat.id,message.text)
 bot.polling(none_stop=True) # начинается отправка запросов на сервера tg и спрашивает нет ли для неё сообщений. Если сообщение есть, то начинается обработка
+'''
 
+bot.polling(none_stop=True) # начинается отправка запросов на сервера tg и спрашивает нет ли для неё сообщений. Если сообщение есть, то начинается обработка
 
